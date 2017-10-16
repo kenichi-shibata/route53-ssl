da0ac20fbca25f99fa57088620e1b2583dcfed5b
Kenichi Shibata <shibata.k@simplex.ne.jp>
2017-10-16 01:49:25 +0000

    Removed default AWS Profile and replaced it with route53 profile to match the documentation in readme.md also added dehydrated auto accept terms

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Creates let's encrypt user on this machine
-./dehydrated --register --accept-terms
 
 RED='\033[0;31m'
 NC='\033[0m'
@@ -11,9 +9,7 @@ echo "This is to generate free ssl cert using ACME Protocol"
 
 echo "checking ACME License agreement"
 if [ ! -d "${PWD}/accounts" ]; then
-    echo -e "please agree to license agreement by running this command by running ${GREEN} ./dehydrated --register --accept-terms ${NC}"
-    echo "available at https://letsencrypt.org/documents/LE-SA-v1.1.1-August-1-2016.pdf"
-    exit 0
+    ./dehydrated --register --accept-terms
 fi
 
 echo -e "Tell me the route53 registered apex domain ${GREEN} [ex. example.com] ${NC}" 
